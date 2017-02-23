52ebbcd014097a465036a85401370531614d0f89
carlgarpel <carlgarpel@gmail.com>
2017-02-23 19:32:51 +0100

    adaptando paso 2

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -20,19 +20,12 @@ var VIAS = [
     { id: 11, tipoVia: 'CALLE', nombreVia: 'BECERRA', distrito: 8, seccion: 6, codigoPostal: '04006' },
     { id: 12, tipoVia: 'CALLE', nombreVia: 'CARRERA DEL DARRO', distrito: 7, seccion: 3, codigoPostal: '04005' },
     { id: 13, tipoVia: 'CALLE', nombreVia: 'DINAMARCA', distrito: 7, seccion: 9, codigoPostal: '04005' },
-    { id: 14, tipoVia: 'AVDA', nombreVia: 'FRANCISCO DE ASÍS', distrito: 7, seccion: 5, codigoPostal: '04003' },
-    { id: 15, tipoVia: 'CALLE', nombreVia: 'GARCILASO DE LA VEGA', distrito: 7, seccion: 1, codigoPostal: '04003' },
-    { id: 16, tipoVia: 'CALLE', nombreVia: 'HUEBRO', distrito: 6, seccion: 2, codigoPostal: '04004' },
-    { id: 17, tipoVia: 'CALLE', nombreVia: 'LORCA', distrito: 4, seccion: 2, codigoPostal: '04005' },
-    { id: 18, tipoVia: 'CALLE', nombreVia: 'MARÍA DE LA O', distrito: 2, seccion: 3, codigoPostal: '04006' },
-    { id: 19, tipoVia: 'CALLE', nombreVia: 'PETENERA', distrito: 7, seccion: 5, codigoPostal: '04007' },
-    { id: 20, tipoVia: 'CALLE', nombreVia: 'RIO DE JANEIRO', distrito: 7, seccion: 4, codigoPostal: '04001' },
-    { id: 21, tipoVia: 'PLAZA', nombreVia: 'DERECHOS HUMANOS', distrito: 4, seccion: 6, codigoPostal: '04003' },
+    { id: 14, tipoVia: 'AVDA', nombreVia: 'FRANCISCO DE ASÍS', distrito: 7, seccion: 5, codigoPostal: '04003' }
 ];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Callejeros';
-        this.vias = VIAS;
+        this.cvias = VIAS;
     }
     AppComponent.prototype.onSelect = function (via) {
         this.selectedVia = via;
@@ -40,7 +33,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <header><h1>{{title}}</h1></header>\n    <div id='main'>\n      <article>\n        <h2>C\u00F3digo: {{via.id}}</h2>\n        <div  class=\"article\"><label>Tipo de v\u00EDa: </label>{{via.tipoVia}}</div>\n        <div> <label>Nombre de v\u00EDa: </label> </div>\n      </article>\n\n      <nav> \n        <label>Listado de v\u00EDas: </label>\n        <ul class=\"vias\">\n          <li *ngFor=\"let via of vias\"\n        [class.selected]=\"via === selectedVia\"\n        (click)=\"onSelect(via)\">\n        <span class=\"badge\">{{via.id}} </span> {{via.nombreVia}} \n      </li>\n    </ul>\n        <input  [(ngModel)]=\"via.nombreVia\" placeholder=\"via\">\n      </nav>\n      <aside>Localizador</aside>\n\n    </div>\n\n     <footer>Vial - 2017</footer>\n\n    "
+            template: "\n    <header><h1></h1></header>\n    <div id='main'>\n      <article  *ngIf=\"selectedVia\">\n        <h2>C\u00F3digo: </h2>\n        <div  class=\"article\"><label>Tipo de v\u00EDa: </label>{{selectedVia.tipoVia}} </div>\n        <div> <label>Distrito: </label>{{selectedVia.distrito}} </div>\n      </article>\n\n      <nav> \n        <label>Listado de v\u00EDas: </label>\n        <ul class=\"vias\">\n          <li *ngFor=\"let via of cvias\"\n       [class.selected]=\"via === selectedVia\"\n        (click)=\"onSelect(via)\">\n        <span class=\"badge\">{{via.id}} </span> {{via.nombreVia}}\n              </li>\n    </ul>\n        \n      </nav>\n     <aside *ngIf=\"selectedVia\">Aqui \n\n          <h2>{{selectedVia.nombreVia}} Detalle</h2>\n          <div><label>id: </label>{{selectedVia.id}}</div>\n          <div>\n            <label>nombre: </label>\n            <input [(ngModel)]=\"selectedVia.nombreVia\" placeholder=\"via\"/>\n          </div>\n\n     </aside>\n    </div>\n\n     <footer>Vial - 2017</footer>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,58 +5,59 @@ export class Via {
   nombreVia: string;
   distrito: number;
   seccion: number;
-  codigoPostal: string
-}
+  codigoPostal: string;
+  }
 
 const VIAS: Via[] = [
-  { id: 10, tipoVia: 'CALLE', nombreVia: 'ALTAMIRA', distrito: 7, seccion: 1, codigoPostal: '04006' },
-  { id: 11, tipoVia: 'CALLE', nombreVia: 'BECERRA', distrito: 8, seccion: 6, codigoPostal: '04006' },
-  { id: 12, tipoVia: 'CALLE', nombreVia: 'CARRERA DEL DARRO', distrito: 7, seccion: 3, codigoPostal: '04005' },
-  { id: 13, tipoVia: 'CALLE', nombreVia: 'DINAMARCA', distrito: 7, seccion: 9, codigoPostal: '04005' },
-  { id: 14, tipoVia: 'AVDA', nombreVia: 'FRANCISCO DE ASÍS', distrito: 7, seccion: 5, codigoPostal: '04003' },
-  { id: 15, tipoVia: 'CALLE', nombreVia: 'GARCILASO DE LA VEGA', distrito: 7, seccion: 1, codigoPostal: '04003' },
-  { id: 16, tipoVia: 'CALLE', nombreVia: 'HUEBRO', distrito: 6, seccion: 2, codigoPostal: '04004' },
-  { id: 17, tipoVia: 'CALLE', nombreVia: 'LORCA', distrito: 4, seccion: 2, codigoPostal: '04005' },
-  { id: 18, tipoVia: 'CALLE', nombreVia: 'MARÍA DE LA O', distrito: 2, seccion: 3, codigoPostal: '04006' },
-  { id: 19, tipoVia: 'CALLE', nombreVia: 'PETENERA', distrito: 7, seccion: 5, codigoPostal: '04007' },
-  { id: 20, tipoVia: 'CALLE', nombreVia: 'RIO DE JANEIRO', distrito: 7, seccion: 4, codigoPostal: '04001' },
-  { id: 21, tipoVia: 'PLAZA', nombreVia: 'DERECHOS HUMANOS', distrito: 4, seccion: 6, codigoPostal: '04003' },
+  {id: 10, tipoVia: 'CALLE', nombreVia: 'ALTAMIRA', distrito: 7, seccion: 1, codigoPostal: '04006' },
+  {id: 11, tipoVia: 'CALLE', nombreVia: 'BECERRA', distrito: 8, seccion: 6, codigoPostal: '04006' },
+  {id: 12, tipoVia: 'CALLE', nombreVia: 'CARRERA DEL DARRO', distrito: 7, seccion: 3, codigoPostal: '04005' },
+  {id: 13, tipoVia: 'CALLE', nombreVia: 'DINAMARCA', distrito: 7, seccion: 9, codigoPostal: '04005' },
+  {id: 14, tipoVia: 'AVDA', nombreVia: 'FRANCISCO DE ASÍS', distrito: 7, seccion: 5, codigoPostal: '04003' }
+ 
 ];
 
 @Component({
   selector: 'my-app',
   template: `
-    <header><h1>{{title}}</h1></header>
+    <header><h1></h1></header>
     <div id='main'>
-      <article>
-        <h2>Código: {{via.id}}</h2>
-        <div  class="article"><label>Tipo de vía: </label>{{via.tipoVia}}</div>
-        <div> <label>Nombre de vía: </label> </div>
+      <article  *ngIf="selectedVia">
+        <h2>Código: </h2>
+        <div  class="article"><label>Tipo de vía: </label>{{selectedVia.tipoVia}} </div>
+        <div> <label>Distrito: </label>{{selectedVia.distrito}} </div>
       </article>
 
       <nav> 
         <label>Listado de vías: </label>
         <ul class="vias">
-          <li *ngFor="let via of vias"
-        [class.selected]="via === selectedVia"
+          <li *ngFor="let via of cvias"
+       [class.selected]="via === selectedVia"
         (click)="onSelect(via)">
-        <span class="badge">{{via.id}} </span> {{via.nombreVia}} 
-      </li>
+        <span class="badge">{{via.id}} </span> {{via.nombreVia}}
+              </li>
     </ul>
-        <input  [(ngModel)]="via.nombreVia" placeholder="via">
+        
       </nav>
-      <aside>Localizador</aside>
+     <aside *ngIf="selectedVia">Aqui 
 
+          <h2>{{selectedVia.nombreVia}} Detalle</h2>
+          <div><label>id: </label>{{selectedVia.id}}</div>
+          <div>
+            <label>nombre: </label>
+            <input [(ngModel)]="selectedVia.nombreVia" placeholder="via"/>
+          </div>
+
+     </aside>
     </div>
 
      <footer>Vial - 2017</footer>
-
     `
 })
  
 export class AppComponent {
   title = 'Callejeros';
-  vias = VIAS;
+  cvias = VIAS;
   selectedVia: Via;
 
   onSelect(via: Via): void {
